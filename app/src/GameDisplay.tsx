fea9ae43a1f5bb7c757f9b98ecf28b08d336a6a1
Patrick Beaume <p.beaume@gmail.com>
2025-04-08 14:10:06 +0200

    add images

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -12,7 +12,7 @@ export const GameDisplay: FC<GameDisplayProps> = () => {
   const margin = { top: 7, left: 0, right: 0, bottom: 0 }
   return (
     <>
-      <GameTable xMin={-60} xMax={60} yMin={-32} yMax={32} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
+      <GameTable xMin={-60} xMax={60} yMin={-32} yMax={32} margin={margin} css={process.env.NODE_ENV !== 'development' && tableBorder}>
         {/*<GameTableNavigation />*/}
         <PlayerPanels />
       </GameTable>
--- rules/src/rules/arcane/description/TheLoveDescription.ts
+++ rules/src/rules/arcane/description/TheLoveDescription.ts
@@ -4,7 +4,7 @@ import { ArcaneEffect } from '../ArcaneEffect'
 
 export class TheLoveDescription extends ArcaneEffect {
   canDiscard(card: ArcaneCard): boolean {
-    if (card !== ArcaneCard.TheLife) return false
+    if (card !== ArcaneCard.TheLove) return false
     return this.table
       .filter((item) => hasColor(item.id, Color.Green))
       .length > 1

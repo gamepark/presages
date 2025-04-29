ee470c95bb9d622b5808b56bfc5a751bc7ba9c44
Patrick Beaume <p.beaume@gmail.com>
2025-04-29 16:45:57 +0200

    fix the anger

--- rules/src/rules/arcane/description/TheAngerDescription.ts
+++ rules/src/rules/arcane/description/TheAngerDescription.ts
@@ -1,10 +1,14 @@
 import { MaterialMove } from '@gamepark/rules-api'
+import { ArcaneCard } from '../../../material/ArcaneCard'
+import { PlayerId } from '../../../PlayerId'
 import { RuleId } from '../../RuleId'
 import { ArcaneEffect } from '../ArcaneEffect'
 
 export class TheAngerDescription extends ArcaneEffect {
-  onPlaceTo(): MaterialMove[] {
+  onPlaceTo(cardId: ArcaneCard, player: PlayerId): MaterialMove[] {
     if (this.table.length === 1) return []
-    return [this.startRule(RuleId.TheAnger)]
+    const cardPlayer = this.table.find((item) => item.id === cardId)!.location.player
+    if (player === cardPlayer) return [this.startRule(RuleId.TheAnger)]
+    return [this.startPlayerTurn(RuleId.TheAnger, cardPlayer!)]
   }
 }

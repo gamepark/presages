7e40a2c5014e4f71040e237ebb17202d65a51cf9
Patrick Beaume <p.beaume@gmail.com>
2025-04-30 13:31:35 +0200

    the dream & the betrayal does not work

--- rules/src/rules/arcane/description/TheDreamDescription.ts
+++ rules/src/rules/arcane/description/TheDreamDescription.ts
@@ -8,6 +8,7 @@ export class TheDreamDescription extends ArcaneEffect {
   canBePlacedInFrontOfOtherPlayers = true
 
   onPlaceTo(_card: ArcaneCard, target: PlayerId): MaterialMove[] {
+    if (this.game.rule?.id === RuleId.TheBetrayal) return []
     if (this.table.length === this.game.players.length) return []
     if (this.player !== target) return [this.startPlayerTurn(RuleId.TheDream, target)]
     return []

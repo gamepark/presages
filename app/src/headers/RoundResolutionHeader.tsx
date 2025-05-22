5719e51b88538fbc3737e5d424d30db1d37a7d40
Romain Fromi <romain.fromi@gmail.com>
2025-05-22 15:46:52 +0200

    English translations

--- app/src/headers/RoundResolutionHeader.tsx
+++ app/src/headers/RoundResolutionHeader.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { ArcaneCard, getColors, isAbsolute } from '@gamepark/presages/material/ArcaneCard'
+import { ArcaneCard, getColors } from '@gamepark/presages/material/ArcaneCard'
 import { Color } from '@gamepark/presages/material/Color'
 import { Memory } from '@gamepark/presages/Memory'
 import { PlayerId } from '@gamepark/presages/PlayerId'
@@ -12,17 +12,18 @@ import { TransComponents } from '../utils/trans.components'
 
 export const RoundResolutionHeader = () => {
   const playerId = usePlayerId()
-  const { t } = useTranslation()
   const rules = useRules<PresagesRules>()!
   const winner = rules.remind<{ player: PlayerId; value: ArcaneCard }>(Memory.WinningCard)
   const itsMe = playerId && winner.player === playerId
   const name = usePlayerName(winner.player)
+  const { t } = useTranslation()
+  const card = winner.value < ArcaneCard.TheAbsolute30 ? t(`card.${winner.value}`) : t(`card.absolute`)
 
   if (itsMe) {
     return (
       <Trans
         defaults="header.round-resolution.me"
-        values={{ value: winner.value, card: t(isAbsolute(winner.value) ? `card.absolute` : `card.${winner.value}`) + ` (${winner.value})` }}
+        values={{ card }}
         components={{
           ...TransComponents,
           color: <span css={colorCss(getColors(winner.value))} />
@@ -34,7 +35,7 @@ export const RoundResolutionHeader = () => {
   return (
     <Trans
       defaults="header.round-resolution.player"
-      values={{ player: name, value: winner.value, card: t(isAbsolute(winner.value) ? `card.absolute` : `card.${winner.value}`) + ` (${winner.value})` }}
+      values={{ player: name, card }}
       components={{
         ...TransComponents,
         color: <span css={colorCss(getColors(winner.value))} />
